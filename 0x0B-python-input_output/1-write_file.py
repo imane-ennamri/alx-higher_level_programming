d2bb15aa7eee605a9feba534f401651858355aca
imane-ennamri <imaneennamri209@gmail.com>
2024-01-09 21:20:04 +0000

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,10 @@
- a file-writing function."""
+#!/usr/bin/python3
+"""Defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
     """Write a string to a UTF8 text file.
+
     Args:
         filename (str): The name of the file to write.
         text (str): The text to write to the file.
