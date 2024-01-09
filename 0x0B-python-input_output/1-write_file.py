d9ea17f333d555a9888ee1cde46c324e6d335d71
imane-ennamri <imaneennamri209@gmail.com>
2024-01-09 20:38:23 +0000

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,6 @@
 
 def write_file(filename="", text=""):
     """Write a string to a UTF8 text file.
-
     Args:
         filename (str): The name of the file to write.
         text (str): The text to write to the file.
